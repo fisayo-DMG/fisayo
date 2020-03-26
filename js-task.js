b7b325fecdb798a746ca9b4dce8126e0362d60df
Fisayo <aikomofisayo@gmail.com>
2020-03-26 23:51:37 +0100

    Add JS task

---
+++ js-task.js
@@ -0,0 +1,19 @@
+let name = "Fisayo Aikomo";
+let course = ["Node JS", "Git", "HTML & CSS", "Javascript"]
+
+console.log(`My name is ${name} and my courses are ${course}`)
+
+let oddNumbers = []
+let evenNumbers
+
+for (let i = 0; i <= 200; i++) {
+    if(i % 2 === 0) {
+        evenNumbers.push(i)
+    }
+    if(i % 2 !== 0) {
+        oddNumbers.push(i)
+    }
+}
+
+console.log(oddNumbers);
+console.log(evenNumbers);
