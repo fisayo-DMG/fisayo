5b262cf2a3cae51834aba5b22f2a77c884f338df
Fisayo <aikomofisayo@gmail.com>
2020-03-27 00:04:44 +0100

    Add Go Task

--- dectobin.go
+++ dectobin.go
@@ -1,3 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var input int64
+	fmt.Println("Please enter an integer")
+	fmt.Scan(&input)
+	fmt.Println(strconv.FormatInt(input, 2))
+}
